d995902b868429fa0b424bea240d19e80cc452ce
Andre Possiedi <andreefps@gmail.com>
2024-04-27 20:44:59 +0200

    chore: typed permissions and roles using zod

---
+++ apps/api/src/index.ts
@@ -0,0 +1,8 @@
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'MEMBER' })
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+const userCanDeleteOtherUser = ability.can('delete', 'User')
+
+console.log(userCanInviteSomeoneElse)
+console.log(userCanDeleteOtherUser)
