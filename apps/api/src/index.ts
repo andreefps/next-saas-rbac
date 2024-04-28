31f40cca3cf3958547990b6dc74da7f9870a9dcd
Andre Possiedi <andreefps@gmail.com>
2024-04-28 17:29:53 +0200

    feat: prisma setup

--- apps/api/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { defineAbilityFor } from '@saas/auth'
-
-const ability = defineAbilityFor({ role: 'MEMBER' })
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUser = ability.can('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUser)
